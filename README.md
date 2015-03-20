9e6a6470f80ce0ad11c65185b35e7d6277a3d407
Nic <nicf@outlook.com>
2015-03-20 14:55:25 -0400

    Updated Readme file

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # nvf5039-ex6
+Exercise for IST 297i class
+Nicholas Freda
+March 20, 2015
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320185001) do
+ActiveRecord::Schema.define(version: 20150320185427) do
 
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
     t.integer  "temperature"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "topic_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
