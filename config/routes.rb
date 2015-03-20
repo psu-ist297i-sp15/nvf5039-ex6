b2c0286ed606e2fcaa51881e15c5a53544e14802
Nic <nicf@outlook.com>
2015-03-20 15:24:42 -0400

    End of class update

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  resources :topics do
+      member do
+          post 'Upvote'
+      end
+  end
   resources :drinks
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150320185427) do
+ActiveRecord::Schema.define(version: 20150320190208) do
 
   create_table "drinks", force: :cascade do |t|
     t.string   "name"
     t.integer  "temperature"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
   end
